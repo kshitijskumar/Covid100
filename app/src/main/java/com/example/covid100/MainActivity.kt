afa7ed5718472ef84ee00a0e10d8f481fcc471a0
Kshitij Kumar <kshitij2212@gmail.com>
2021-05-14 14:40:13 +0530

    splash screen added

--- app/src/main/java/com/example/covid100/MainActivity.kt
+++ app/src/main/java/com/example/covid100/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Covid100)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
